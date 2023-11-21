0a7cc1e7fe383f76fd8066d51f0596919a24b042
vinoth_technotackle <vinod.kambil@technotackle.com>
2023-11-21 22:16:05 +0530

    .

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/CustomDataModelBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDataModelBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by TechnoTackle on 21/11/23.
+//
+
+import SwiftUI
+
+struct CustomDataModelBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomDataModelBootCamp()
+}
